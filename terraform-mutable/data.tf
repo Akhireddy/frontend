0b7e00263e89ce8dff92996cd60608fc93f0a3ab
Akhireddy <akhir1989@gmail.com>
2022-01-23 09:59:57 +0800

    var file updated with cidr

--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -11,7 +11,6 @@ data "terraform_remote_state" "vpc" {
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
-}
 
 data "terraform_remote_state" "alb" {
   backend = "s3"
@@ -20,7 +19,21 @@ data "terraform_remote_state" "alb" {
     key    = "mutable/alb/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
+
+resource "aws_lb_target_group" "tg"
+    name      = "${var.ENV}-${var.COMPONENT}"
+    port      = 80
+    protocol  = "HTTP"
+    vpc_id    = data.terraform_remote_state.vpc.outputs.VPC_ID
 }
+
+resource "aws_lb_target_group_attachment" "tg-attach" {
+    count             = length(aws_spot_instance_request.ec2-spot)
+    target_group_arm  =  aws_lb_target_group.tg.arm
+    target_id         = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
+    port              = 80
+}
+
 
 data "aws_secretsmanager_secret" "common" {
   name = "common/ssh"
